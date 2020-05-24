d99003bb2304c2e51e69f49c5da6c6c49ef635e8
tech-camp <tech-camp@A-108.local>
2020-05-24 21:09:22 +0900

    createerror

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,24 +1,50 @@
 $(function () {
   function buildHTML(message) {
-    
-    var image = message.image ? `<img src=${message.image}>` : "";
+    // 画像・テキストの変数
+    var image = message.image ? `<img src=${message.image} class='main__chats__chat__bottom__image'>` : "";
+    var text = message.message ? `<p class="main__chats__chat__text__comment">${message.message}</p>` : "";
 
-    var text = message.message ? `
-    <div class="main__chats__chat__text">
-      <p class="main__chats__chat__text__comment">${message.message}</p>
-    </div>` : "";
-
+    // htmlの変数
     var html = `<div class="main__chats__chat" data-message-id=${message.id}>
-          <div class="main__chats__chat__upper">
-            <div class="main__chats__chat__upper__talker">${message.user_name}</div>
-            <div class="main__chats__chat__upper__date">${message.created_at}</div>
-          </div>
-          ${text}
-          ${image}
-        </div>`;
+                  <div class="main__chats__chat__upper">
+                    <div class="main__chats__chat__upper__talker">${message.user_name}</div>
+                    <div class="main__chats__chat__upper__date">${message.created_at}</div>
+                  </div>
+                  <div class='main__chats__chat__bottom'>
+                    ${text}
+                    ${image}
+                  </div>
+                </div>`;
     return html;
   }
 
+  // メッセージの非同期投稿の関数
+  $("#new_message").on("submit", function (e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr("action");
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
+      .done(function (data) {
+        var html = buildHTML(data);
+        $(".main__chats").append(html)
+        $(".main__chats").animate({ scrollTop: $(".main__chats")[0].scrollHeight });
+        $("form")[0].reset();
+        $(".submit-save").removeAttr("disabled");
+      })
+      .fail(function () {
+        alert("メッセージ送信に失敗しました");
+      });
+  });
+
+  // 自動更新の関数
   var reloadChats = function () {
     var last_message_id = $(".main__chats__chat:last").data("message-id");
     var group_id = $(".main__header__leftbox__groupname").data("group-id")
@@ -43,31 +69,6 @@ $(function () {
       alert("error");
     });
   };
-
-  $("#new_message").on("submit", function (e) {
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr("action");
-
-    $(".main__form__box__submit").removeAttr("data-disable-with");
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: "json",
-      processData: false,
-      contentType: false
-    })
-      .done(function (data) {
-        var html = buildHTML(data);
-        $(".main__chats").append(html)
-        $(".main__chats").animate({ scrollTop: $(".main__chats")[0].scrollHeight });
-        $("form")[0].reset();
-      })
-      .fail(function () {
-        alert("メッセージ送信に失敗しました");
-      });
-  });
 
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
     setInterval(reloadChats, 7000);
